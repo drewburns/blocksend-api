2657ba1340ae9651129050d40d7970bafec4fb36
drewburns <drewburnsbab@gmail.com>
2022-03-22 19:08:33 -0400

    better env and v1ish

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "andrewburns",
+    password: "",
+    database: "blockreward_dev",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_DATABASE,
+    host: process.env.DB_PROD_HOST,
+    dialect: "postgres",
+  },
+};
