ef1ca3f627be0e6842beda763f95e49bf5f36711
drewburns <drewburnsbab@gmail.com>
2022-04-09 18:07:04 -0400

    config pptions

--- config/config.js
+++ config/config.js
@@ -20,6 +20,9 @@ module.exports = {
     host: process.env.DB_PROD_HOST,
     psqlString: process.env.DATABASE_URL,
     dialect: "postgres",
+    ssl: {
+      rejectUnauthorized: false,
+    },
     dialectOptions: {
       ssl: {
         require: true,
